47cf84425dca0f59ad943404102d1aaa1b8fd596
wbrowne <wbrowne@tcd.ie>
2020-03-21 15:47:48 +0100

    Read story json file into struct

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
-func main()  {
-	fmt.Println("Hello world!")
+type Story map[string]PlotPoint
+
+type PlotPoint struct {
+	Title   string    `json:"title"`
+	Story   []string  `json:"story"`
+	Options []PlotOptions `json:"options"`
+}
+
+type PlotOptions struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+func main() {
+	story := getJsonFileAsMap("gopher.json")
+
+	fmt.Print(story)
+}
+
+func getJsonFileAsMap(filename string) Story {
+	content, err := ioutil.ReadFile(filename)
+
+	var story Story
+	err = json.Unmarshal(content, &story)
+	if err != nil {
+		panic(err)
+	}
+
+	return story
 }
