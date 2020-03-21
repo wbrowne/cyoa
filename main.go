54a22f3e7767973cc23e699b7791c3ef5755b597
wbrowne <wbrowne@tcd.ie>
2020-03-21 16:27:18 +0100

    Add template file and test render the intro

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
 type Story map[string]PlotPoint
@@ -22,7 +24,13 @@ type PlotOptions struct {
 func main() {
 	story := getJsonFileAsMap("gopher.json")
 
-	fmt.Print(story)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		tmpl := template.Must(template.ParseFiles("plot-point.html"))
+		tmpl.Execute(w, story["intro"])
+	})
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func getJsonFileAsMap(filename string) Story {
